305f8a0e1303a4da785b1d5d466a6d4052082a1e
ParkJunyeong <49705282+heffy127@users.noreply.github.com>
2019-06-20 15:02:33 +0900

    Create BoardGoodDTO.java

---
+++ src/board/BoardGoodDTO.java
@@ -0,0 +1,18 @@
+package board;
+
+public class BoardGoodDTO {
+	private String user;
+	private int board_num;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public int getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(int board_num) {
+		this.board_num = board_num;
+	}
+}
